71f07487bf7aca9ccd3d41f0cec69d82f52f0b34
beepscore <support@beepscore.com>
2012-03-11 13:42:58 -0700

    Implement both_ends().

--- basic/string1.py
+++ basic/string1.py
@@ -37,8 +37,11 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    # +++your code here+++
-    return
+    if len(s) < 2 :
+        string_ends = ''
+    else:
+        string_ends = s[:2] + s[-2:]
+    return string_ends
 
 
 # C. fix_start

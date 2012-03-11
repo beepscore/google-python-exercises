45c53888dfb044294f5afbf9f26b907c431fdd7f
beepscore <support@beepscore.com>
2012-03-11 13:51:00 -0700

    Implement fix_start().

--- basic/string1.py
+++ basic/string1.py
@@ -54,8 +54,10 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # +++your code here+++
-    return
+    s_first = s[0]
+    s_end = s[1:]
+    s_end_replaced = s_end.replace(s_first, '*')
+    return s_first + s_end_replaced
 
 
 # D. MixUp

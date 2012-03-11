e509eff65c423be031b00033ceeb6feb761db115
beepscore <support@beepscore.com>
2012-03-11 14:08:21 -0700

    Implement verbing().

--- basic/string2.py
+++ basic/string2.py
@@ -16,8 +16,15 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            ending = 'ly'
+        else:
+            ending = 'ing'
+    else:
+        ending = ''
 
-    return
+    return s + ending
 
 
 # E. not_bad

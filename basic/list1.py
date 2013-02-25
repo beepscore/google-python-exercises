063a1e4de9dcb4c19f823a8ecbab8310af6f9adb
beepscore <support@beepscore.com>
2013-02-24 21:34:44 -0800

    In list1.py use word[-1] to get last element.

--- basic/list1.py
+++ basic/list1.py
@@ -24,7 +24,7 @@ def match_ends(words):
     count = 0
     for word in words:
         if ((len(word) >= 2)
-            and (word[0] == word[len(word)-1])):
+            and (word[0] == word[-1])):
             count += 1
 
     return count

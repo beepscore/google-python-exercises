28ffa7b07666891f97b91daed0283708f5d56c8f
beepscore <support@beepscore.com>
2013-03-08 19:41:45 -0800

    In babynames.py fix reindent. In vim change file settings to indent 4 spaces and reindent.

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -9,7 +9,8 @@
 import sys
 import re
 
-"""Baby Names exercise
+"""
+Baby Names exercise
 
 Define the extract_names() function below and change main()
 to call it.
@@ -17,22 +18,22 @@ to call it.
 For writing regex, it's nice to include a copy of the target
 text for inspiration.
 
-Here's what the html looks like in the baby.html files:
-    ...
-    <h3 align="center">Popularity in 1990</h3>
-    ....
-    <tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
-    <tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
-    <tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
-    ...
+Here's what the html looks like in the baby.html files
+...
+<h3 align="center">Popularity in 1990</h3>
+....
+<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
+<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
+<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
+...
 
-Suggested milestones for incremental development:
-    -Extract the year and print it
-    -Extract the names and rank numbers and just print them
-    -Get the names data into a dict and print it
-    -Build the [year, 'name rank', ... ] list and print it
-    -Fix main() to use the extract_names list
-    """
+Suggested milestones for incremental development
+-Extract the year and print it
+-Extract the names and rank numbers and just print them
+-Get the names data into a dict and print it
+-Build the [year, 'name rank', ... ] list and print it
+-Fix main() to use the extract_names list
+"""
 
 def extract_names(filename):
     """
@@ -42,7 +43,7 @@ def extract_names(filename):
     """
     # +++your code here+++
 
-  return
+    return
 
 
 def main():
@@ -51,19 +52,19 @@ def main():
     # which is the script itself.
     args = sys.argv[1:]
 
-  if not args:
-      print 'usage: [--summaryfile] file [file ...]'
-      sys.exit(1)
+    if not args:
+        print 'usage: [--summaryfile] file [file ...]'
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-      summary = True
-      del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
 
 if __name__ == '__main__':
     main()

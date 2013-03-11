76286c1f9521cc63ff7220fa2c9fd8b49ee72bb9
beepscore <support@beepscore.com>
2013-03-10 21:00:56 -0700

    In copyspecial.py change indentation from 2 to 4 spaces by changing vim file settings and re-indenting.

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -19,51 +19,52 @@ import commands
 # +++your code here+++
 # Write functions and modify main() to call them
 def get_special_paths(dir):
-"""
-return a list of the absolute paths of the special files in the given directory
-"""
+    """
+    return a list of the absolute paths of the special files in the given directory
+    """
 
 def copy_to(paths, dir):
-"""
-given a list of paths, copies those files into the given directory
-"""
+    """
+    given a list of paths, copies those files into the given directory
+    """
 
 def zip_to(paths, zippath):
-"""
-given a list of paths, zip those files up into the given zipfile
-"""
+    """
+    given a list of paths, zip those files up into the given zipfile
+    """
 
 
 def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
+    # This basic command line argument parsing code is provided.
+    # Add code to call your functions below.
 
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
-    sys.exit(1)
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
+    if not args:
+        print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+        sys.exit(1)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    # todir and tozip are either set from command line
+    # or left as the empty string.
+    # The args array is left just containing the dirs.
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+    tozip = ''
+    if args[0] == '--tozip':
+        tozip = args[1]
+        del args[0:2]
 
-  if len(args) == 0:
-    print "error: must specify one or more dirs"
-    sys.exit(1)
+    if len(args) == 0:
+        print "error: must specify one or more dirs"
+        sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
+    # +++your code here+++
+    # Call your functions
+    get_special_paths(todir)
 
 if __name__ == "__main__":
-  main()
+    main()

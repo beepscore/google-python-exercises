9dea80ef0e82de71bb39066301c2c120614f6d13
beepscore <support@beepscore.com>
2013-04-28 22:36:06 -0700

    Implement test_read_urls() to expect stubbed results, implement read_urls() to return a stub result so that test passes.

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -25,6 +25,7 @@ def read_urls(filename):
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
     # +++your code here+++
+    return ['10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"']
 
 
 def download_images(img_urls, dest_dir):
--- logpuzzle/test_logpuzzle.py
+++ logpuzzle/test_logpuzzle.py
@@ -13,7 +13,29 @@ class TestCopySpecial(unittest.TestCase):
 
 
     def test_read_urls(self):
-        pass
+
+        results = logpuzzle.read_urls('a_filename')
+
+        expected_result = 1
+        self.assertEqual(expected_result, len(results),
+                         'expected {} but got {}'.format(
+                             expected_result,
+                             len(results)))
+
+        test_index = 0
+        expected_result_index = 1
+
+        test_datas = [
+            [0, '10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"']
+        ]
+
+        for test_data in test_datas:
+            result = results[test_data[test_index]]
+            expected_result = test_data[expected_result_index]
+            self.assertEqual(expected_result, result,
+                             'read_urls()[{}] expected {} but got {}'.format(test_index,
+                                                                                 expected_result,
+                                                                                 result))
 
 
     def test_download_images(self):

77515c6a4289b321ee2b8539135f2071efd12ab3
beepscore <support@beepscore.com>
2013-04-28 20:29:44 -0700

    Add test_logpuzzle.py

---
+++ logpuzzle/test_logpuzzle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# References
+# http://docs.python.org/3.3/library/unittest.html
+
+import unittest
+import logpuzzle
+
+class TestCopySpecial(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_read_urls(self):
+        pass
+
+
+    def test_download_images(self):
+        pass
+
+if __name__ == "__main__": unittest.main()
